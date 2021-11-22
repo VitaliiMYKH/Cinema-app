5899852416d9ecb83eb75ecc91dbe826634eec69
Vitalii Mykh <vitaliymykh@gmail.com>
2021-11-22 14:03:24 +0200

    working project

--- src/main/java/mykh/vitalii/spring/model/ShoppingCart.java
+++ src/main/java/mykh/vitalii/spring/model/ShoppingCart.java
@@ -9,7 +9,7 @@ import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "shopping_carts")
+/*@Table(name = "shopping_carts")*/
 public class ShoppingCart {
     @Id
     private Long id;
